6876d1497d7ce9994f7d920ea99950415968ba2d
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-10-17 17:46:31 -0500

    Updates

---
+++ precache-manifest.a9690f4037d99a0a2afbee1f26bf3c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "5f3eeee12fa0974ddc8c",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.5f3eeee1.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "5f3eeee12fa0974ddc8c",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.d5bb68b9.chunk.css"
+  },
+  {
+    "revision": "e6471ecc74e1011d69baed11f2ff57db",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
